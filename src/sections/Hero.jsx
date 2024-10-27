d1bbf182e0a0f1e64a011088987040cdb185b356
Mosin Balsing <mosinbalsing@gmail.com>
2024-10-27 20:22:05 +0530

    previous changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,13 +4,12 @@ import { motion } from 'framer-motion';
 import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-  const isTablet = useMediaQuery({ query: "(min-width: 768px) and (max-width: 1024px)" });
-
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobileOrTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
   return (
-    <section className={`min-h-screen w-full flex ${isMobile || isTablet ? 'flex-col' : 'flex-row'} relative`}>
-      <div className="w-full sm:w-[50%] mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 z-20 relative">
+    <section className={`min-h-screen w-full flex ${isMobileOrTablet ? 'flex-col' : 'flex-row'} relative`}>
+      <div className="w-full sm:w-[50%] mx-auto flex flex-col sm:mt-36 mt-[30%] c-space gap-3 z-20 relative">
         <motion.p
           className="text-left hero_tag text-gray_gradient"
           initial={{ opacity: 0, y: -20 }}
@@ -31,24 +30,48 @@ const Hero = () => {
         </motion.p>
       </div>
 
-      <div className={`w-full  flex ${ isTablet ? 'flex-row items-center justify-center  w-[100%]' : isMobile ? 'flex-col items-center w-[100%] ' : 'flex-row'} mt-20 gap-3 z-20 relative`}>
-        {/* Display images in a column on mobile and tablet view, side-by-side on larger screens */}
-        <motion.img
-          src="assets/hero.png"
-          alt="fabrication"
-          className={`w-[80%] h-auto rounded border-white ${ isTablet ? 'mb-4 w-[45%]' : isMobile ?'w-[40%] h-[50%]': 'w-[40%] h-[50%]'} `}
-          initial={{ opacity: 0, y: isMobile || isTablet ? -30 : -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-        />
-        <motion.img
-          src="assets/hero2.png"
-          alt="fabrication"
-          className={`w-[80%] h-auto rounded border-white  ${ isTablet ? 'mb-4 w-[45%]' : isMobile ?'w-[40%] h-[50%]': 'w-[40%] h-[50%]'}  `}
-          initial={{ opacity: 0, y: isMobile || isTablet ? 30 : 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
-        />
+      <div className={`w-[50%] ${isMobileOrTablet ? 'flex-col w-full' : 'flex-row'} flex justify-center items-center sm:mt-36 mt-20 gap-3 z-20 relative`}>
+        {isMobileOrTablet ? (
+          <>
+            {/* Display images in a column on mobile and tablet view */}
+            <motion.img
+              src="assets/hero.png"
+              alt="fabrication"
+              className="w-[80%] h-auto rounded border-white mb-4"
+              initial={{ opacity: 0, y: -30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, ease: "easeOut" }}
+            />
+            <motion.img
+              src="assets/hero2.png"
+              alt="fabrication"
+              className="w-[80%] h-auto rounded border-white mb-4"
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
+            />
+          </>
+        ) : (
+          <>
+            {/* Display images side-by-side on larger screens */}
+            <motion.img
+              src="assets/hero.png"
+              alt="fabrication"
+              className="w-[40%] h-[50%] rounded border-white"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, ease: "easeOut" }}
+            />
+            <motion.img
+              src="assets/hero2.png"
+              alt="fabrication"
+              className="w-[40%] h-[50%] rounded border-white mb-[20%] ml-[5%] top-0"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
+            />
+          </>
+        )}
       </div>
 
       {/* Bottom button section */}
