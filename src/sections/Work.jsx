3a81988a3de78fda0760a09c837ba26e9261fe87
Mosin Balsing <mosinbalsing@gmail.com>
2024-11-10 14:14:24 +0530

    Adding Photos in Assests

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -1,11 +1,23 @@
-import { section } from "framer-motion/client";
 import React from "react";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Work = () => {
   return (
-    <section className={`min-h-screen w-full flex relative`}>
-      
+    <section className={`min-h-screen w-full flex relative justify-center items-center`}>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
     </section>
   );
 };
