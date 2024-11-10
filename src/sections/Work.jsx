ebc8585a93047391121cf9c4e0da056ec2ba3406
Mosin Balsing <mosinbalsing@gmail.com>
2024-11-10 16:45:41 +0530

    update id

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Work = () => {
   return (
-    <section className={` w-full flex flex-col relative justify-center items-center`}>
+    <section className={` w-full flex flex-col relative justify-center items-center`} id="work">
        <h1 className="text-3xl font-semibold mb-2 text-white font-generalsans">Work</h1>
       <Gallery />
     </section>
