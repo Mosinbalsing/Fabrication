ab4e3402ac155fe9e7596cf6f2220606885d2ffd
Mosin Balsing <mosinbalsing@gmail.com>
2024-11-24 21:06:04 +0530

    margine added

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -5,7 +5,7 @@ import { styles } from "../style";
 
 const Work = () => {
   return (
-    <section className={` w-full flex flex-col relative justify-center items-center`} id="work">
+    <section className={` w-full flex flex-col relative justify-center items-center mt-11`} id="work">
        <h1 className={`${styles.sectionHeadText} text-blue-500`}>Work</h1>
       <Gallery />
     </section>
